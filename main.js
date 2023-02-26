8257f4410272caf108833c84ab2678bc5c827d50
Kobe-deve <koberunnels@gmail.com>
2023-02-25 22:21:09 -0600

    ball added

--- main.js
+++ main.js
@@ -34,9 +34,16 @@ paddleWidth = 100
 paddleHeight = 50
 y = screen.height*6/8-paddleHeight
 
+ballX = 10
+ballY = 10
+ballRadius = 5
+ballVelocityX = 10
+ballVelocityY = 10
+
+// mouse input for paddle
 function paddleInput(event)
 {
-	onmousemove = console.log(screen.width + " " + screen.height);
+	//onmousemove = console.log(screen.width + " " + screen.height);
 	
 	x = event.clientX-paddleWidth/2
 	
@@ -52,7 +59,34 @@ function inputHandler(event)
 // handles in-game logic 
 function logicHandling()
 {
+	console.log(ballX + " " + ballY + " " + ballVelocityX + " " + ballVelocityY)
 	
+	// ball logic 
+	if(ballX-ballRadius > 0 && ballX+ballRadius < screen.width)
+		ballX += ballVelocityX
+	else
+	{
+		if(ballX+ballRadius >= screen.width)
+			ballX = screen.width
+		else
+			ballX = 0
+		
+		ballVelocityX = -ballVelocityX
+		ballX += ballVelocityX
+	}
+	
+	if(ballY-ballRadius > 0 && ballY+ballRadius < screen.height)
+		ballY += ballVelocityY
+	else
+	{
+		if(ballY+ballRadius >= screen.height)
+			ballY = screen.height
+		else
+			ballY = 0
+			
+		ballVelocityY = -ballVelocityY
+		ballY += ballVelocityY
+	}
 }
 
 // displaying to the screen 
@@ -61,6 +95,12 @@ function draw()
 	// clear screen
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	
+	// draw assets
+	ctx.beginPath();
+	ctx.arc(ballX, ballY, ballRadius, 0, 2 * Math.PI, false)
+	ctx.stroke();	
+	
+	// draw player 
 	ctx.beginPath();
 	ctx.rect(x, y, paddleWidth, paddleHeight);
 	ctx.stroke();	
